a4b69d8b9fd8d8bb7838b9c79f3a5620018f59b2
prog03 <prog03@RC002.mshome.net>
2015-08-18 14:52:59 +0500

    Merge origin/master

---
+++ orenrock/src/java/orenrock/view/ConstantsBean.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package orenrock.view;
+import orenrock.utils.Constants;
+
+/**
+ *
+ * @author prog03
+ */
+public class ConstantsBean {
+
+    
+    public String getSiteTitle(){
+        return Constants.SITE_TITLE;
+    }
+    
+    public ConstantsBean() {
+    }
+    
+}
